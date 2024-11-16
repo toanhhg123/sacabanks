7054ea8ebe8dd9df2c4caa8a26a9700b1eb4b254
rolle_dev <89302745+toanhhg123@users.noreply.github.com>
2024-11-16 15:31:25 +0700

    chore: test

--- src/main/java/com/project/sacabank/auth/service/AuthService.java
+++ src/main/java/com/project/sacabank/auth/service/AuthService.java
@@ -1,7 +1,6 @@
 package com.project.sacabank.auth.service;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
